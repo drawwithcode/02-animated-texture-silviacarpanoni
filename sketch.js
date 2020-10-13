aa82e88a7600cdc95a84d368575fe43eed9f0c12
silviacarpanoni <71377279+silviacarpanoni@users.noreply.github.com>
2020-10-13 17:03:48 +0200

    drops
    animated texture

--- sketch.js
+++ sketch.js
@@ -1,12 +1,41 @@
-function preload(){
-  // put preload code here
+const colorPalette = ["SteelBlue", "IndianRed", "LightSkyBlue", "Salmon"];
+const d = 50;
+const a = 20;
+
+function preload() {
 }
+
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  frameRate(4);
+  noStroke();
 }
 
+
 function draw() {
-  // put drawing code here
+  background("LightSkyBlue");
+
+  for(let x=a; x<=width; x+=d) {
+    for(let y=a; y<=height; y+=d) {
+
+    if (mouseIsPressed) {
+      if (mouseX<windowWidth && mouseY<windowHeight) {
+          fill(color(random(colorPalette)));
+  }
+}
+
+      ellipse(x,y,d);
+      square(x,y,25);
+
+
+      push();
+      noStroke();
+      textFont("Helvetica");
+      fill("black");
+      textSize(20);
+      text("click on screen to change pattern", 750, 220);
+      pop();
+    }
+  }
 }
